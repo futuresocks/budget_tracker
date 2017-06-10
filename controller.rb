0b5e0a82a5322acdafe8f188f46ef3200df71b2b
Colin Bell <colin@dogoodercomics.co.uk>
2017-06-10 15:38:30 +0100

    Budget viewer working

---
+++ controller.rb
@@ -0,0 +1,21 @@
+require ('sinatra')
+require ('sinatra/contrib/all')
+require ('pry-byebug')
+
+require_relative ('models/users')
+require_relative ('models/transactions')
+
+get '/' do
+  @users = User.all
+  erb(:budget)
+end
+
+get '/transactions' do
+  @transactions = Transaction.all
+  erb(:transactions)
+end
+
+get '/transactions/new' do
+  @transactions = Transaction.all
+  erb(:new_transactions)
+end
